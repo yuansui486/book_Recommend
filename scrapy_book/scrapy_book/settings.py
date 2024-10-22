94c058d4a69d0efdd0b156631c63c0ec2aec2a83
Su yuan <zxc872202364@163.com>
2024-10-22 13:00:02 +0800

    fix 作者、评价人数、价格爬取；添加cookie；添加限速

--- scrapy_book/scrapy_book/settings.py
+++ scrapy_book/scrapy_book/settings.py
@@ -15,6 +15,11 @@ SPIDER_MODULES = ['scrapy_book.spiders']
 NEWSPIDER_MODULE = 'scrapy_book.spiders'
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.8 Safari/537.36'
+# 添加 COOKIE
+COOKIES_ENABLED = True  # 确保启用 Cookies
+DEFAULT_REQUEST_HEADERS = {
+    'Cookie': 'll="108302"; bid=Es1RecnG3AY; douban-fav-remind=1; ap_v=0,6.0; viewed="1915958_1019568_1068920_4820710_4913064_24531956"; dbcl2="255790192:uWQQlJ2pRKY"; ck=shus; push_noty_num=0; push_doumail_num=0; frodotk_db="18dc04cc0c647a5b4a940e64252d6f11"; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1729573003%2C%22https%3A%2F%2Fbook.douban.com%2Fsubject%2F2070844%2F%22%5D; _pk_id.100001.8cb4=579a79fb500cd9bc.1729573003.; _pk_ses.100001.8cb4=1'
+}
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scrapy_book (+http://www.yourdomain.com)'
@@ -28,7 +33,11 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+#延迟
+DOWNLOAD_DELAY = 2  # 每2秒请求一次
+AUTOTHROTTLE_ENABLED = True  # 启用自动限速
+AUTOTHROTTLE_START_DELAY = 1  # 初始延迟时间
+AUTOTHROTTLE_MAX_DELAY = 5  # 最大延迟时间
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- scrapy_book/scrapy_book/spiders/spider_xpath.py
+++ scrapy_book/scrapy_book/spiders/spider_xpath.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 from scrapy_book.items import ScrapyBookItem
 
 
@@ -12,36 +12,69 @@ class SpiderForXPath(scrapy.Spider):
             url = 'https://book.douban.com/top250?start={}'.format(a * 25)
             yield scrapy.Request(url=url, callback=self.parse)
 
-
     def parse(self, response):
-        items = []
-
         for book in response.xpath('//*[@id="content"]/div/div[1]/div/table'):
             item = ScrapyBookItem()
 
+            # 书名和副标题
             title1 = book.xpath("./tr/td[2]/div[1]/a/@title").extract_first().replace('\n', '').strip()
-            title2 = "无" if book.xpath("./tr/td[2]/div[1]/span/text()").extract_first() == None else book.xpath(
+            title2 = "无" if book.xpath("./tr/td[2]/div[1]/span/text()").extract_first() is None else book.xpath(
                 "./tr/td[2]/div[1]/span/text()").extract_first().replace('\n', '').strip()
             item['title'] = title1 + "(" + title2 + ")"
-            item['s_img'] = book.xpath("./tr/td[1]/a/img/@src").extract_first().replace('\n', '').strip()
-            item['scrible'] = "无" if book.xpath("./tr/td[2]/p[2]/span/text()").extract_first() == None else book.xpath(
-                "./tr/td[2]/p[2]/span/text()").extract_first().replace('\n', '').strip()
-            sub_url = book.xpath("./tr/td[2]/div/a/@href").extract_first().replace('\n', '').strip()
-            items.append(item)
 
-            # meta={"item":item} 传递item引用SinaItem对象
+            # 小图
+            item['s_img'] = book.xpath("./tr/td[1]/a/img/@src").extract_first().replace('\n', '').strip()
+
+            # 图书简介
+            item['scrible'] = "无" if book.xpath("./tr/td[2]/p[2]/span/text()").extract_first() is None else book.xpath(
+                "./tr/td[2]/p[2]/span/text()").extract_first().replace('\n', '').strip()
+
+            # 作者、出版社、出版日期、价格
+            book_info = book.xpath("./tr/td[2]/p[1]/text()").extract_first().replace('\n', '').strip().split('/')
+            if len(book_info) == 4:
+                item['author'] = book_info[0].strip()
+                item['publisher'] = book_info[1].strip()
+                item['pub_date'] = book_info[2].strip()
+                item['price'] = book_info[3].strip()
+            else:
+                item['author'] = item['publisher'] = item['pub_date'] = item['price'] = "未知"
+
+            # 评分
+            item['score'] = book.xpath("./tr/td[2]/div[2]/span[2]/text()").extract_first().strip()
+
+            # 评价人数
+            num_text = book.xpath("./tr/td[2]/div[2]/span[3]/text()").extract_first()
+            if num_text:
+                item['num'] = re.sub(r'\D', '', num_text)  # 只提取数字部分
+            else:
+                item['num'] = "未知"
+
+            # 详情页URL
+            sub_url = book.xpath("./tr/td[2]/div/a/@href").extract_first().replace('\n', '').strip()
+
             yield scrapy.Request(url=sub_url, callback=self.parse_second, meta={"item": item})
 
     def parse_second(self, response):
         item = response.meta["item"]
-        item["category_id"] = ""
-        item["category"] = ""
-        # book = response.xpath('//div[@class="indent"]/div').extract_first()
-        # item["author"] = book.xpath("./div[1]/a[1]/text()").extract_first().replace('\n', '').strip()
-        # item["publisher"] = book.xpath("./div[1]/a/@href").extract_first().replace('\n', '').strip()
-        # item["pub_date"] = book.xpath("./div[1]/a/@href").extract_first().replace('\n', '').strip()
-        # item["price"] = book.xpath("./div[1]/a/@href").extract_first().replace('\n', '').strip()
-        # item["m_img"] = book.xpath("./div[1]/a/@href").extract_first().replace('\n', '').strip()
-        # item["b_img"] = book.xpath("./div[1]/a/@href").extract_first().replace('\n', '').strip()
-        # item["isbn"] = book.xpath("./div[2]/a[1]/text()").extract_first().replace('\n', '').strip()
+
+        # 修改后的作者提取逻辑
+        author = response.xpath('//span[contains(text(),"作者")]/following-sibling::a/text()').extract_first()
+        if author:
+            # 清理掉作者字段中的换行符和空格
+            item["author"] = re.sub(r'\s+', ' ', author).strip()
+        else:
+            item["author"] = "未知"
+
+        # 出版社
+        publisher = response.xpath('//span[text()="出版社:"]/following-sibling::a/text()').extract_first()
+        item["publisher"] = publisher.strip() if publisher else "未知"
+
+        # 出版日期
+        pub_date = response.xpath('//span[text()="出版年:"]/following-sibling::text()').extract_first()
+        item["pub_date"] = pub_date.strip() if pub_date else "未知"
+
+        # 价格
+        price = response.xpath('//span[text()="定价:"]/following-sibling::text()').extract_first()
+        item["price"] = price.strip() if price else "未知"
+
         yield item
